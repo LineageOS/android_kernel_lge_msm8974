25153dddb4a7991e431ffc7adf7afba6eda2a609
Al Viro <viro@zeniv.linux.org.uk>
2012-08-12 17:18:05 -0400

    make get_unused_fd_flags() a function
    ... and get_unused_fd() a macro around it
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Change-Id: Iaa2b6f5f2bb9cd3feeddb9e47b144dc0554a2fe1

--- fs/file.c
+++ fs/file.c
@@ -478,8 +478,8 @@ out:
 }
 EXPORT_SYMBOL(alloc_fd);
 
-int get_unused_fd(void)
+int get_unused_fd_flags(unsigned flags)
 {
-	return alloc_fd(0, 0);
+	return alloc_fd(0, flags);
 }
-EXPORT_SYMBOL(get_unused_fd);
+EXPORT_SYMBOL(get_unused_fd_flags);
