9ee88028658696248500fe4b3fb2ccacd0b69bb5
Kees Cook <keescook@chromium.org>
2014-06-10 15:40:23 -0700

    ARM: add seccomp syscall
    Wires up the new seccomp syscall.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    
    Conflicts:
    	arch/arm/include/uapi/asm/unistd.h
    	arch/arm/kernel/calls.S
    
    Change-Id: I31a2d38b892e2cd81bf3998a916c7bb539a37767

--- arch/arm/include/asm/unistd.h
+++ arch/arm/include/asm/unistd.h
@@ -404,6 +404,7 @@
 #define __NR_setns			(__NR_SYSCALL_BASE+375)
 #define __NR_process_vm_readv		(__NR_SYSCALL_BASE+376)
 #define __NR_process_vm_writev		(__NR_SYSCALL_BASE+377)
+#define __NR_seccomp			(__NR_SYSCALL_BASE+383)
 
 //FEATURE_SDCARD_MEDIAEXN_SYSTEMCALL_ENCRYPTION[S]
 #define __NR_set_media_property	(__NR_SYSCALL_BASE+378)
