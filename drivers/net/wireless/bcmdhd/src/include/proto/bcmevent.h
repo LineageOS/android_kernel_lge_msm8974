16023a5256bd9f3df2538e6c5f56be603afa0624
Ashwin <ashwin.bhat@broadcom.com>
2015-04-03 13:46:31 -0700

    net: wireless: bcmdhd: Roam enhancements/autojoin
    Changes to support Android M roam enhancements/autojoin
    offload in FW
    
    Change-Id: Iea17a89a477aa4d3d13a4b59c779e3c54d4eabf5
    Signed-off-by: Ashwin <ashwin.bhat@broadcom.com>

--- drivers/net/wireless/bcmdhd/src/include/proto/bcmevent.h
+++ drivers/net/wireless/bcmdhd/src/include/proto/bcmevent.h
@@ -234,7 +234,8 @@ typedef BWL_PRE_PACKED_STRUCT struct bcm_event {
 
 #define WLC_E_RMC_EVENT			139	/* RMC event */
 #define WLC_E_PFN_SSID_EXT      142  /* SSID EXT event */
-#define WLC_E_LAST			143	/* highest val + 1 for range checking */
+#define WLC_E_ROAM_EXP_EVENT    143  /* Expanded roam event */
+#define WLC_E_LAST			144	/* highest val + 1 for range checking */
 
 #if (WLC_E_LAST > 140)
 #error "WLC_E_LAST: Invalid value for last event; must be <= 140."
