329b5bcbe1358141ebccb993398464cffd0d409e
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2015-10-27 21:41:43 +0530

    wlan:Check priviledge permission before processing SET_CHAR_GET_NONE IOCTL
    Kernel assumes all SET IOCTL commands are assigned with even
    numbers. But in our WLAN driver, some SET IOCTLS are assigned with
    odd numbers. This leads kernel fail to check, for some SET IOCTLs,
    whether user has the right permission to do SET operation.
    Hence, in driver, before processing SET_CHAR_GET_NONE IOCTLs, making
    sure user task has right permission to process the command.
    
    Change-Id: I7b060bcdc84f7016e8d301e994437a535533a260
    CRs-Fixed: 930935

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
@@ -4880,6 +4880,13 @@ static int __iw_setchar_getnone(struct net_device *dev,
 #endif /* WLAN_FEATURE_VOWIFI */
     struct iw_point s_priv_data;
 
+    if (!capable(CAP_NET_ADMIN))
+    {
+      VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+		FL("permission check failed"));
+      return -EPERM;
+    }
+
     if ((WLAN_HDD_GET_CTX(pAdapter))->isLogpInProgress)
     {
         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_FATAL,
