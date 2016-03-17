db708c50b203c31d0282c7b36c5b354b1023afdc
Mukul Sharma <mukul@qti.qualcomm.com>
2016-03-17 10:28:36 -0700

    wlan:Check priviledge permission for SET_VAR_INTS_GETNONE IOCTL
    Kernel assumes all SET IOCTL commands are assigned with even
    numbers. But in our WLAN driver, some SET IOCTLS are assigned with
    odd numbers. This leads kernel fail to check, for some SET IOCTLs,
    whether user has the right permission to do SET operation.
    Hence, in driver, before processing SET_VAR_INTS_GETNONE, making
    sure user task has right permission to process the command.
    
    Bug: 27104184
    Change-Id: Ia2465433aab6366160a167a62ca03e0ba720bcdb
    Signed-off-by: Yuan Lin <yualin@google.com>

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
@@ -5965,6 +5965,13 @@ static int __iw_set_var_ints_getnone(struct net_device *dev,
     int staId = 0;
     int ret = 0;
 
+    if (!capable(CAP_NET_ADMIN))
+    {
+      VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+		FL("permission check failed"));
+      return -EPERM;
+    }
+
     if (extra == NULL)
     {
         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
