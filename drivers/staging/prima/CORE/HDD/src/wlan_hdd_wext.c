9fbb8028ce5486634e2ed202b4ae91ec6bef9fba
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2016-03-17 10:54:37 -0700

    wlan:Check priviledge permission
    for SET_THREE_INT_GET_NONE
    
    Kernel assumes all SET IOCTL commands are assigned with even
    numbers. But in our WLAN driver, some SET IOCTLS are assigned with
    odd numbers. This leads kernel fail to check, for some SET IOCTLs,
    whether user has the right permission to do SET operation.
    Hence, in driver, before processing SET_THREE_INT_GET_NONE IOCTL,
    making sure user task has right permission to process the command.
    
    Bug: 27104184
    Change-Id: I8661872786adfb5492da505ba3960e62064ddd7e
    Signed-off-by: Yuan Lin <yualin@google.com>

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
@@ -5145,6 +5145,13 @@ int __iw_set_three_ints_getnone(struct net_device *dev,
     int sub_cmd = value[0];
     int ret = 0;
 
+    if (!capable(CAP_NET_ADMIN))
+    {
+      VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+		FL("permission check failed"));
+      return -EPERM;
+    }
+
     if ((WLAN_HDD_GET_CTX(pAdapter))->isLogpInProgress)
     {
         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_FATAL,
