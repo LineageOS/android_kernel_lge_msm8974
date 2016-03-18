650eb17edf154e78d9c5ca2dc99e01ebb23a756d
Mukul Sharma <mukul@qti.qualcomm.com>
2016-03-17 17:00:41 -0700

    wlan:Check priviledge permission
    for SET_POWER_PARAMS IOCTL
    
    Kernel assumes all SET IOCTL commands are assigned with even
    numbers. But in our WLAN driver, some SET IOCTLS are assigned with
    odd numbers. This leads kernel fail to check, for some SET IOCTLs,
    whether user has the right permission to do SET operation.
    Hence, in driver, before processing SET_POWER_PARAMS IOCTL, making
    sure user task has right permission to process the command.
    
    Bug: 27104184
    Change-Id: Iaab3d55c2acc75f65d6daf5998713cc9ff92a32c
    Signed-off-by: Yuan Lin <yualin@google.com>

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
@@ -8142,10 +8142,20 @@ static int __iw_set_power_params_priv(struct net_device *dev,
                                       struct iw_request_info *info,
                                       union iwreq_data *wrqu, char *extra)
 {
+
   int ret;
   char *ptr;
+
   VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
                 "Set power params Private");
+
+  if (!capable(CAP_NET_ADMIN))
+  {
+    VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+	      FL("permission check failed"));
+    return -EPERM;
+  }
+
   /* ODD number is used for set, copy data using copy_from_user */
   ptr = mem_alloc_copy_from_user_helper(wrqu->data.pointer,
                                           wrqu->data.length);
